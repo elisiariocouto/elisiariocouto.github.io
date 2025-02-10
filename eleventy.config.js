204cb3b5b0ce9a6ab5398d62d1a0e1aca8ef1448
Elisiário Couto <elisiario@couto.io>
2025-02-10 01:03:16 +0000

    feat: Add introduction on home, PGP key and security.txt.

--- eleventy.config.js
+++ eleventy.config.js
@@ -3,6 +3,8 @@ import { feedPlugin } from "@11ty/eleventy-plugin-rss";
 import pluginSyntaxHighlight from "@11ty/eleventy-plugin-syntaxhighlight";
 import pluginNavigation from "@11ty/eleventy-navigation";
 import { eleventyImageTransformPlugin } from "@11ty/eleventy-img";
+import { EleventyRenderPlugin } from "@11ty/eleventy";
+
 
 import pluginFilters from "./_config/filters.js";
 
@@ -105,6 +107,8 @@ export default async function (eleventyConfig) {
 	eleventyConfig.addShortcode("currentBuildDate", () => {
 		return (new Date()).toISOString();
 	});
+
+	eleventyConfig.addPlugin(EleventyRenderPlugin);
 
 	// Features to make your build faster (when you need them)
 
